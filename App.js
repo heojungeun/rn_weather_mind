96dc5014ed0d8bcb14a82b2471159a9f72153ded
heojungeun <heocube@naver.com>
2021-10-31 23:21:20 +0900

    initial screen - city, temp, desc

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window")
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView 
+        horizontal 
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>26</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>26</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>26</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>26</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </ScrollView>
+      <StatusBar style='light'></StatusBar>
     </View>
   );
 }
@@ -14,8 +41,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'rgb(115, 119, 158)',
+  },
+  city: {
+    flex: 1.2,
+    // color: 'white',
+    // backgroundColor: "rgb(222, 224, 236)",
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '500',
+  },
+  weather: {
+    // backgroundColor: "rgb(72, 73, 82)",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+    // backgroundColor: "teal",
+  },
+  temp: {
+    fontSize: 158,
+  },
+  desc: {
+    fontSize: 60,
+    marginTop: -30,
   },
 });
---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Weather app with mind
+
+### 노마드 코더 챌린지🧑🏻‍💻
+[ ] 날씨 앱 완성
+[ ] 1일 1 마음 날씨 체크 기능
+[ ] 캘린더 확인 기능
+
+##### 참조 🧑🏻‍🎤
++ 리액트 네이티브 Document: [reactnative.dev](https://reactnative.dev/docs/components-and-apis)
++ Expo API Reference: [docs.expo.dev](https://docs.expo.dev/versions/latest/)
